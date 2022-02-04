e1c846e4949bf256f5f78da5fd575bae5bc629a9
Marcos Alexandria <93044961+marcosalexandria@users.noreply.github.com>
2022-02-03 21:44:22 -0300

    commit01

---
+++ app-vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header/>
+    <Corpo/>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  import Corpo from './components/Corpo.vue'
+export default {
+  name:'App',
+  components:{
+    Header,
+    Corpo
+  }
+}
+</script>
---
+++ app-vue/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <h1>Triângulo em VueJS</h1>
+        <p>Adicione 3 vaores ao sistema, para que ele retorne o tipo de triângulo que os valores formaram</p>
+        <hr>
+    </section>
+</template>
+
+<script>
+
+export default {
+    name:'Header'
+}
+</script>
+
+<style scoped>
+section{
+    text-align: center;
+    font-size: 1.2em;
+}
+</style>
