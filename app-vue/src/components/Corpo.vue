b50be86c7e01e3090a060631323f653bb2155984
Marcos Alexandria <93044961+marcosalexandria@users.noreply.github.com>
2022-02-04 20:14:08 -0300

    commit2

--- app-vue/src/components/Corpo.vue
+++ app-vue/src/components/Corpo.vue
@@ -1,12 +1,60 @@
 <template>
     <section>
-        
+        <div>
+            <input type="number" id="num1">
+        </div>
+        <div>
+            <input type="number" id="num2">
+        </div>
+        <div>
+            <input type="number" id="num3">
+        </div>
+        <div>
+            <button @click="verificar" id="bnt">Enviar</button>
+        </div>
+        <div id="res">
+
+        </div>
+        <div>
+            <img :src="imagem" alt="">
+        </div>
     </section>
 </template>
 
 <script>
 
 export default {
-    name:'Corpo'
+    name:'Corpo',
+    data(){
+        return{
+            imagem:''
+        }
+    },
+    methods:{
+        verificar(){
+            let num1=document.getElementById("num1").value;
+            let num2=document.getElementById("num2").value;
+            let num3=document.getElementById("num3").value;
+            let res=document.getElementById("res");
+            
+            if((num1==num2&&num1!=num3)||(num2==num3&&num2!=num1)||(num1==num3&&num1!=num2)){
+                res.innerHTML=`Com as medidas ${num1}, ${num2} e ${num3}, podemos montar um triângulo Isósceles`;
+            }else if((num1!=num2&&num1!=num3)||(num2!=num1&&num2!=num3)||(num3!=num1&&num3!=num2)){
+                res.innerHTML=`Com as medidas ${num1}, ${num2} e ${num3}, podemos montar um triângulo Escaleno`
+            }else if(num1==num2&&num1==num3){
+                res.innerHTML=`Com as medidas ${num1}, ${num2} e ${num3}, podemos montar um triângulo Equilátero`
+            }
+        }
+    }
 }
 </script>
+
+<style scoped>
+section{
+    text-align: center;
+}
+
+div{
+    margin: 10px;
+}
+</style>
